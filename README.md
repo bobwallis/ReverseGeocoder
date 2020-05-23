9051d546a4ecfafbe2d8afadd3cdeb884160ef26
Bob Wallis <bob.wallis@gmail.com>
2020-05-23 23:13:32 +0100

    PHP reverse geocoder

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ReverseGeocoder
-A simple reverse geocoder
+# Reverse Geocoder
+
+Reverse geocoding is the process of coding of a point location (latitude, 
+longitude) to a readable address or place name. This is a simple one designed 
+for self-hosting.
+
+## Data
+
+The reverse geocoder uses a database table with the following columns:
+
+| Column    | Data Type    | Description                          |
+|-----------|--------------|--------------------------------------|
+| id        | integer      | ID column                            |
+| country   | char(2)      | ISO-3166 2-letter country code       |
+| name      | text         | Name of geographical point           |
+| latitude  | decimal(8,3) | Latitude in decimal degrees (WGS84)  |
+| longitude | decimal(8,3) | Longitude in decimal degrees (WGS84) |
+
+You can get this from any source you like. In the `./data` folder there is a
+script that help with generating such a table using cc-by licenced data from
+[GeoNames](http://www.geonames.org/export/).
+
+
+## Using
+
+Open `./reverse_geocode.php` and edit the Data Source Name at the top so the 
+script can connect to the data you just created.
+
+Upload to your PHP server of choice, and access like this:
+`https://example.com/reverse_geocode.php?lat=51.7546&lon=-1.2588`
+
+Porting to other languages is an exercise for the reader.
---
+++ reverse_geocode.php
@@ -0,0 +1,82 @@
+<?php
+$dsn      = 'sqlite:/mnt/storage2/ReverseGeocoder/data/data.sqlite';
+$username = null;
+$password = null;
+$prod     = false;
+
+// Get lat and lon from $_GET, and validate them
+if( !isset( $_GET['lat'] ) || !isset( $_GET['lon'] ) ) {
+    http_response_code( 400 );
+    die();
+}
+$lat = floatval( $_GET['lat'] );
+$lon = floatval( $_GET['lon'] );
+if( $lat < -90 || $lat > 90 || $lon < -180 || $lon > 180 ) {
+    http_response_code( 400 );
+    die();
+}
+
+
+// Connect to database
+try {
+    $pdo = !is_null($username)? new PDO( $dsn, $username, $password ) : new PDO ( $dsn );
+} catch (PDOException $e) {
+    http_response_code( 500 );
+    if( !$prod ) {
+        echo 'Connection failed: ' . $e->getMessage();
+    }
+    die();
+}
+
+
+// Get a 10km bounding box
+$bbx_size_km  = 5;
+$bbox_min_lat = $lat - ($bbx_size_km / 111.045);
+$bbox_max_lat = $lat + ($bbx_size_km / 111.045);
+$bbox_min_lon = $lon - ($bbx_size_km / (111.045 * cos(deg2rad($lat))));
+$bbox_max_lon = $lon + ($bbx_size_km / (111.045 * cos(deg2rad($lat))));
+if( $bbox_min_lat < -90 )  $bbox_min_lat = ($bbox_min_lat + 180)*-1;
+if( $bbox_max_lat >  90 )  $bbox_max_lat = ($bbox_max_lat - 180)*-1;
+if( $bbox_min_lon < -180 ) $bbox_min_lon = ($bbox_min_lon + 360);
+if( $bbox_max_lon >  180 ) $bbox_max_lon = ($bbox_max_lon - 360);
+
+
+// Get the points inside it
+try {
+    $sql = 'SELECT
+                p.name,
+                p.country,
+                p.latitude,
+                p.longitude 
+            FROM place p
+            WHERE
+                p.latitude > :bbox_min_lat AND p.latitude < :bbox_max_lat
+                AND
+                p.longitude > :bbox_min_lon AND p.longitude < :bbox_max_lon;';
+    $sth = $pdo->prepare( $sql );
+    $sth->execute( compact( 'bbox_min_lat', 'bbox_max_lat', 'bbox_min_lon', 'bbox_max_lon' ) );
+    $result = $sth->fetchAll( PDO::FETCH_ASSOC );
+} catch (PDOException $e) {
+    http_response_code( 500 );
+    if( !$prod ) {
+        echo 'Query failed: ' . $e->getMessage();
+    }
+    die();
+}
+
+
+// Of those points, rank by distance to the requested lat/lon
+$closest = array_reduce( $result, function( $carry, $item ) use ($lat, $lon) {
+    $item['distance'] = asin(sqrt(pow(sin((deg2rad($lat) - deg2rad($item['latitude'])) / 2), 2) + cos(deg2rad($item['latitude'])) * cos(deg2rad($lat)) * pow(sin((deg2rad($lon) - deg2rad($item['longitude'])) / 2), 2)));
+    return $item['distance'] < $carry['distance']? $item : $carry;
+}, array( 'name' => $lat.', '.$lon, 'distance' => 999 ) );
+unset( $closest['distance'] );
+unset( $closest['latitude'] );
+unset( $closest['longitude'] );
+
+
+// Output
+header( 'Content-Type: application/javascript' );
+header( 'Cache-Control: public,max-age=604800' );
+http_response_code( 200 );
+echo json_encode( $closest );
